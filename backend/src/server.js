849626f9a3e8b45af99d88bd22d3016f9ff91f8e
ANKITH33 <ankithkini43@gmail.com>
2025-12-25 09:43:16 +0530

    added middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,31 @@ import routes from "./routes/routes.js";
 import {connectDB} from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();//helps read the env file
 const app = express();
 const PORT=process.env.PORT || 5001;
 
-connectDB();
-app.use(cors());
-app.use(express.json());
 
-app.use("/teams", routes);
+app.use(cors());
+app.use(express.json());// this middleware will parse json bodies : req.body
+
+app.use((req,res,next) => {
+    console.log(`Request method is ${req.method} and request url is ${req.url}`);
+    next();
+})
+
+app.use(rateLimiter);
+
+app.use("/", routes);
 //app.use("/products", productroutes);
-app.listen(PORT, () => {
-    console.log("Server is running on port 5001");
-});
+
+connectDB().then(()=>{
+    app.listen(PORT, () => {
+        console.log("Server is running on port 5001");
+    });
+})//programming practice we need to connect to db first
+
 
 //mongodb+srv://ankithkini43_db_user:SB9fL8h4AoAPFGmW@cluster0.ftm12yk.mongodb.net/?appName=Cluster0
