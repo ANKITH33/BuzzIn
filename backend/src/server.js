8d7f96194709c6ed881db0b9d384480296635266
ANKITH33 <ankithkini43@gmail.com>
2026-01-10 16:02:05 +0530

    Fixing issues

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,7 +22,7 @@ const PORT=process.env.PORT || 5001;
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://buzz-in-six.vercel.app",
+    "https://buzz-in-coral.vercel.app",
     "https://buzz-difv6uqhd-ankith33s-projects.vercel.app",
   ],
   
@@ -48,7 +48,7 @@ connectDB().then(()=>{
     cors: {
         origin: [
         "http://localhost:5173",
-        "https://buzz-in-six.vercel.app",
+        "https://buzz-in-coral.vercel.app",
         "https://buzz-difv6uqhd-ankith33s-projects.vercel.app",
         ],
         methods: ["GET", "POST"],
