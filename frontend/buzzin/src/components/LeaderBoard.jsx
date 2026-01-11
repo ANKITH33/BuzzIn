06945484211a455145e844dfbf67dfcd23b434ba
ANKITH33 <ankithkini43@gmail.com>
2026-01-11 19:51:39 +0530

    Minor UI changes

--- frontend/buzzin/src/components/LeaderBoard.jsx
+++ frontend/buzzin/src/components/LeaderBoard.jsx
@@ -3,12 +3,13 @@ import LeaderBoardEntry from './LeaderBoardEntry';
 
 const Leaderboard = ({teams=[]}) => {
   return (
-    <div className="bg-yellow-500 opacity-90 rounded-xl w-full p-3 sm:p-4 flex flex-col items-center">
-        <h1 className="text-slate-950 font-medium text-2xl mb-5 md:mb-2 md:mx-6 mt-2 text-center md:text-left w-full">
+    <div className="bg-yellow-500 opacity-90 rounded-xl w-full p-3 sm:p-4 flex flex-col">
+        <h1 className="text-slate-950 font-medium text-2xl mb-5 mt-2 text-center md:text-left w-full md:ml-6 md:mr-6">
           Leaderboard
         </h1>
+
         
-        <div className="w-full flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-2 md:ml-6 md:mr-6">
         {teams.map((team,i) =>(
           <LeaderBoardEntry key={team._id}
             rank={i+1}
