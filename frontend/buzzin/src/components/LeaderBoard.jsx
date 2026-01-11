39dc404956434f2ed34ca8b9538816c13bf139b1
ANKITH33 <ankithkini43@gmail.com>
2026-01-11 19:55:54 +0530

    Minor UI changes

--- frontend/buzzin/src/components/LeaderBoard.jsx
+++ frontend/buzzin/src/components/LeaderBoard.jsx
@@ -3,23 +3,26 @@ import LeaderBoardEntry from './LeaderBoardEntry';
 
 const Leaderboard = ({teams=[]}) => {
   return (
-    <div className="bg-yellow-500 opacity-90 rounded-xl w-full p-3 sm:p-4 flex flex-col">
-        <h1 className="text-slate-950 font-medium text-2xl mb-5 mt-2 text-center md:text-left w-full md:ml-6 md:mr-6">
+    <div className="bg-yellow-500 opacity-90 rounded-xl w-full p-3 sm:p-4">
+      <div className="md:mx-6">
+        <h1 className="text-slate-950 font-medium text-2xl mt-2 mb-3 text-center md:text-left">
           Leaderboard
         </h1>
 
-        
-        <div className="w-full flex flex-col gap-2 md:ml-6 md:mr-6">
-        {teams.map((team,i) =>(
-          <LeaderBoardEntry key={team._id}
-            rank={i+1}
-            name={team.teamName}
-            score={team.totalScore}
+        <div className="flex flex-col gap-2">
+          {teams.map((team, i) => (
+            <LeaderBoardEntry
+              key={team._id}
+              rank={i + 1}
+              name={team.teamName}
+              score={team.totalScore}
             />
-        ))}
+          ))}
         </div>
+      </div>
     </div>
-  )
+  );
+
 }
 
 export default Leaderboard
