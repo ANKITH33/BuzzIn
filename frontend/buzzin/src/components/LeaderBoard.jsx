8ca75bdbe51675d778f75faf32222dc681d11376
ANKITH33 <ankithkini43@gmail.com>
2026-01-11 19:46:45 +0530

    Minor UI changes

--- frontend/buzzin/src/components/LeaderBoard.jsx
+++ frontend/buzzin/src/components/LeaderBoard.jsx
@@ -4,7 +4,7 @@ import LeaderBoardEntry from './LeaderBoardEntry';
 const Leaderboard = ({teams=[]}) => {
   return (
     <div className="bg-yellow-500 opacity-90 rounded-xl w-full p-3 sm:p-4 flex flex-col items-center">
-        <h1 className="text-slate-950 font-medium text-2xl mb-5 mt-2 text-center md:text-left w-full">
+        <h1 className="text-slate-950 font-medium text-2xl mb-5 md:mb-2 md:mx-6 mt-2 text-center md:text-left w-full">
           Leaderboard
         </h1>
         
